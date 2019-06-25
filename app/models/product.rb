f0a76e748e3cf11d706acd2cd441c912749949cf
shownola <swasieko@gmail.com>
2019-06-25 11:42:07 -0700

    Add cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
-
+  before_destroy :not_referenced_by_any_line_item
   mount_uploader :image
   # If you use SQLite use this serialize
    # serialize :image
 
    belongs_to :user, optional: true
+   has_many :line_items
 
    validates :title, :sku, :price, :cost, presence: true
    validates :description, length: { maximum: 1000, too_long: "%{count} characters allowed" }
@@ -12,6 +13,13 @@ class Product < ApplicationRecord
    validates :price, numericality: {only_decimal: true}, length: { maximum: 7 }
 
 
+   private
 
+    def not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line items present')
+        throw :abort
+      end
+    end
 
 end
